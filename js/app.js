1235b58e7c46827c6081a9f753ab89154c2a94cd
Nate Lynch <natelynch@Nates-MacBook-Pro.local>
2020-08-07 17:16:25 -0400

    linking JS and CSS

---
+++ js/app.js
@@ -0,0 +1,19 @@
+/*----- constants -----*/
+
+console.log('hello');
+
+/*----- app's state (variables) -----*/
+
+
+
+/*----- cached element references -----*/
+
+
+
+/*----- event listeners -----*/
+
+
+
+/*----- functions -----*/
+
+
