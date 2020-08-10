2d93ec14bc2d99db1ee09a9a953295c33d146a7b
Nate Lynch <natelynch@Nates-MBP.lan>
2020-08-10 11:41:20 -0400

    adding final html and some js

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ const value = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 
 
 /*----- app's state (variables) -----*/
-
+let money;
 
 
 /*----- cached element references -----*/
