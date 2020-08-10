d3ecf509818eb8d6d08f083ca8b1bc9151c3263a
Nate Lynch <natelynch@Nates-MBP.lan>
2020-08-10 13:51:00 -0400

    declaring state and caching elements

--- js/app.js
+++ js/app.js
@@ -3,13 +3,22 @@ const suits = ['s', 'c', 'd', 'h'];
 
 const value = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
 
+const dealerMin = 17;
+
 
 /*----- app's state (variables) -----*/
 let money;
 
+let playerValue;
+
+let dealerValue;
 
 /*----- cached element references -----*/
+const msg = document.getElementById('msg');
 
+const dealerCards = document.getElementById('dealer-cards');
+
+const playerCards = document.getElementById('player-cards');
 
 
 /*----- event listeners -----*/
