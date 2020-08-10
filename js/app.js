365774ae5beb51d0a557243f4013497888b0195d
Nate Lynch <natelynch@Nates-MBP.lan>
2020-08-09 22:02:36 -0400

    declaring constants

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 /*----- constants -----*/
+const suits = ['s', 'c', 'd', 'h'];
 
+const value = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
 
 
 /*----- app's state (variables) -----*/
